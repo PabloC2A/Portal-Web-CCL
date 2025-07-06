353cd2c0365ef52729135794a1250bcd17c439b6
PabloC2A <pabloandrescriolloalvarez@gmail.com>
2025-07-06 13:38:54 -0500

    Configuracion Inical

---
+++ core/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index, name='index'),
+    path('nosotros/', views.nosotros, name='nosotros'),
+    path('servicios/', views.servicios, name='servicios'),
+    path('noticias/', views.noticias, name='noticias'),
+    path('contacto/', views.contacto, name='contacto'),
+]
---
+++ core/views.py
@@ -0,0 +1,16 @@
+from django.shortcuts import render
+
+def index(request):
+    return render(request, 'core/index.html')
+
+def nosotros(request):
+    return render(request, 'core/nosotros.html')
+
+def servicios(request):
+    return render(request, 'core/servicios.html')
+
+def noticias(request):
+    return render(request, 'core/noticias.html')
+
+def contacto(request):
+    return render(request, 'core/contacto.html')
